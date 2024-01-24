387757106f17e097360593e4a985abc6d2cd7bfd
Abdul Hadi <abdulhadi.reality@gmail.com>
2024-01-24 23:38:59 +0500

    follow not working

--- client/components/SuggestionsWidget/Suggestion.jsx
+++ client/components/SuggestionsWidget/Suggestion.jsx
@@ -2,9 +2,37 @@ import React from 'react'
 import Image from 'next/image'
 import { CiCirclePlus } from "react-icons/ci";
 import Link from 'next/link';
+import { useFollowUser } from '@/utils/Hooks/UseHooks';
 import server from '@/utils/server';
+import { notification } from 'antd'
 
 function Suggestion({ user }) {
+    const { followUser } = useFollowUser()
+
+    const handleFollow = async () => {
+        try {
+            const res = await followUser(user?._id)
+            if (res?.data?.success) {
+                notification.success({
+                    message: 'Success',
+                    description: res?.data?.message
+                })
+            } else {
+                notification.error({
+                    message: 'Error',
+                    description: res?.data?.message
+                })
+            }
+        } catch (error) {
+            console.log(error)
+            notification.error({
+                message: 'Error',
+                description: error?.response?.data?.message
+            })
+        }
+    }
+
+
     return (
         <div className='flex'>
             <Link href={`/profile/${user?._id}`}>
@@ -15,7 +43,7 @@ function Suggestion({ user }) {
             <div className='ml-3'>
                 <p className='font-semibold'>{user?.fName} {user?.lName}</p>
                 <p className='text-gray-500 text-sm'>{user?.userName}</p>
-                <div className='cursor-pointer text-sm px-3 py-1 my-2 w-[50px] border-solid border-blue-500 text-blue-500 rounded-full font-semibold hover:bg-blue-500 hover:text-white transition-all flex items-center'>
+                <div onClick={handleFollow} className='cursor-pointer text-sm px-3 py-1 my-2 w-[50px] border-solid border-blue-500 text-blue-500 rounded-full font-semibold hover:bg-blue-500 hover:text-white transition-all flex items-center'>
                     Follow
                 </div>
             </div>
