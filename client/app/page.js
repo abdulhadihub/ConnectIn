c83e5805dc8196a4c59eaa0ae7d6092a688371a2
Abdul Hadi <abdulhadi.reality@gmail.com>
2024-01-25 12:43:07 +0500

    login and registration

--- client/app/page.js
+++ client/app/page.js
@@ -5,18 +5,25 @@ import Image from 'next/image'
 function Page() {
   return (
     <>
-      <div className='bg-slate-100 h-[90vh] flex flex-col justify-center items-center'>
+      <div className='bg-slate-100 '>
 
-        <div>
+        <div className='grid grid-cols-2'>
 
+          <div className='col-span-2 lg:col-span-1'>
 
-          <div>
-            <h1 className='text-center text-3xl '>Welcome to your <br /> community</h1>
-            <div className='w-[250px] bg-white px-10 py-5 rounded-lg shadow-md my-5'>
-              <Login />
+            <div className='flex flex-col justify-center items-center my-5'>
+              <h1 className='text-center text-3xl '>Welcome to your <br /> community</h1>
+              <div className='w-[250px] bg-white px-10 py-5 rounded-lg shadow-md my-5'>
+                <Login />
+              </div>
             </div>
           </div>
-          <Image src='/connections.jpg' width={500} height={500} />
+
+          <div className='col-span-2 lg:col-span-1'>
+            <div className='flex justify-center items-center'>
+              <Image src='/connections.svg' width={500} height={500} className='w-[300px] h-[300px] lg:w-[500px] lg:h-[500px]' />
+            </div>
+          </div>
         </div>
       </div>
     </>
--- client/app/register/page.jsx
+++ client/app/register/page.jsx
@@ -1,16 +1,26 @@
 import React from 'react'
 import Register from '@/components/Forms/Register'
+import Image from 'next/image'
 
 
 function Page() {
     return (
         <>
-            <div className='bg-slate-100 flex flex-col justify-center items-center'>
-
-                <h1 className='text-center text-2xl my-5'>Make the most of your life,<br /> make connections</h1>
-                <div className='md:w-[400px] flex justify-center items-center bg-white px-3 py-5 rounded-lg shadow-md my-5'>
-
-                    <Register />
+            <div className='bg-slate-100 '>
+                <div className='grid grid-cols-2'>
+                    <div className='col-span-2 lg:col-span-1'>
+                        <div className='flex flex-col justify-center items-center my-5'>
+                            <h1 className='text-center text-2xl my-5'>Make the most of your life,<br /> make connections</h1>
+                            <div className='w-[250px] bg-white px-10 py-5 rounded-lg shadow-md my-5'>
+                                <Register />
+                            </div>
+                        </div>
+                    </div>
+                    <div className='col-span-2 lg:col-span-1'>
+                        <div className='flex justify-center items-center'>
+                            <Image src='/connections-registration.svg' width={500} height={500} className='w-[300px] h-[300px] lg:w-[500px] lg:h-[500px]' />
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
