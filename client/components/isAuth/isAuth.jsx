2c433de7990d288f4c8f889f510ae9f5c1790139
Abdul Hadi <abdulhadi.reality@gmail.com>
2024-01-25 12:51:17 +0500

    added isAuth

---
+++ client/components/isAuth/isAuth.jsx
@@ -0,0 +1,49 @@
+"use client";
+import { useUser } from "@/utils/Context/UserContext";
+import { useEffect } from "react";
+import { redirect } from "next/navigation";
+import { useCookies } from 'react-cookie';
+import axios from 'axios';
+import server from '@/utils/server';
+
+export default function IsAuth(Component) {
+    return function IsAuth(props) {
+        const [cookies] = useCookies(['x-auth-token']);
+        const { user, updateUser } = useUser();
+        const token = cookies["x-auth-token"];
+
+        const fetchUser = async () => {
+            try {
+                const res = await axios.get(`${server}/api/user/verify`, {
+                    headers: {
+                        'x-auth-token': cookies['x-auth-token']
+                    },
+                });
+                if (res?.data?.success) {
+                    updateUser(res?.data?.user);
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+
+        useEffect(() => {
+            if (!user) {
+                if (cookies["x-auth-token"]) {
+                    fetchUser();
+                }
+                else {
+                    return redirect("/login");
+                }
+            }
+        }, [token, user]);
+
+
+        if (!user) {
+            return null;
+        }
+
+        return <Component {...props} />;
+    };
+}
